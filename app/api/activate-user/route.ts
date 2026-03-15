32a31cc5f35e54deb570b8ed20ed23426dc459b6
thanhliemchg <ntliemit@gmail.com>
2026-03-16 00:24:33 +0700

    fix activate user api

--- app/api/activate-user/route.ts
+++ app/api/activate-user/route.ts
@@ -1,16 +1,18 @@
+import { NextResponse } from "next/server"
 import { supabase } from "@/lib/supabase"
-import { error } from "console"
 
-export async function POST(req:Request){
+export async function POST(req: Request) {
 
-const {id} = await req.json()
+  const { id } = await req.json()
 
-await supabase
-.from("users")
-.update({status:"active"})
-.eq("id",id)
+  const { error } = await supabase
+    .from("users")
+    .update({ status: "active" })
+    .eq("id", id)
 
-if (error) return Response.json({error})
-return Response.json({success:true})
+  if (error) {
+    return NextResponse.json({ error: error.message })
+  }
 
+  return NextResponse.json({ success: true })
 }
